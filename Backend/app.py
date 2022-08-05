96cfe79256fdc28fc427a66b2b4a87726d4312d4
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-08-05 09:47:26 -0500

    feat: added 'partners' endpoint

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,6 @@
-import os 
-import sys
-from flask import Flask, jsonify
-
+from flask import Flask, request, jsonify, send_file
+import utils.database as db
+import utils.processing as proc
 
 app = Flask(__name__)
 app.secret_key = 'very-secret-key'
@@ -11,6 +10,13 @@ app.secret_key = 'very-secret-key'
 def index():
     response = {'message': 'Hello World!'}
     return jsonify(response)
+
+
+@app.route('/partners', methods=['GET'])
+def query():
+    query = "SELECT * FROM res_partner"
+    df = proc.get(query)
+    return df.to_json(orient='records')
 
 
 if __name__ == '__main__':
