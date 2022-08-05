91309f4a12f1739958fcb74da4bf1432111c7511
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-08-05 09:48:46 -0500

    feat: added query-to-dataframe function

---
+++ Backend/utils/processing.py
@@ -0,0 +1,8 @@
+import utils.database as db
+import pandas.io.sql as psql
+
+def get(query):
+    connection = db.connect()
+    df = psql.read_sql(query, connection)
+    connection.close()
+    return df
