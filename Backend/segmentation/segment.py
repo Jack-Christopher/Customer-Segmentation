af4a62f851f90878a3420dd039159b2d75795efa
LuisSante <lsantet@unsa.edu.pe>
2022-08-05 14:16:18 -0500

    feat: processing database data nan

--- Backend/segmentation/segment.py
+++ Backend/segmentation/segment.py
@@ -6,4 +6,17 @@ def QUERY():
     segment_ds = get(query)
     return segment_ds
 
-print(QUERY())
+dataset = QUERY()
+
+dataset['probability'] = dataset['probability'].fillna(dataset['probability'].mean())
+dataset['contact_name'] = dataset['contact_name'].fillna("Luis Sante")
+dataset['expected_revenue'] = dataset['expected_revenue'].fillna(0)
+
+for i in range(len(dataset['expected_revenue'])):
+    if(dataset['expected_revenue'][i] == 0):
+        dataset['expected_revenue'][i] = dataset['expected_revenue'][i] + dataset['probability'].mean()
+
+print(dataset.isnull().sum())
+
+print(dataset)
+
