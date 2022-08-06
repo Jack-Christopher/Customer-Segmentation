298a9f5db3b9892701c2c8d0492e1d3139179242
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-08-06 13:15:04 -0500

    chore: Moved functions to Backend/segmentation/segment.py

--- Backend/segmentation/processing.py
+++
@@ -1,8 +0,0 @@
-import database as db
-import pandas.io.sql as psql
-
-def get(query):
-    connection = db.connect()
-    df = psql.read_sql(query, connection)
-    connection.close()
-    return df
