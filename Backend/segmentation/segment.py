ce1e3566e806b6ff429e7f26dd64279e966e41e3
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-08-14 10:57:40 -0500

    feat: improved data preprocessing and data retrieval

--- Backend/segmentation/segment.py
+++ Backend/segmentation/segment.py
@@ -4,7 +4,7 @@ import pandas.io.sql as psql
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
-import database as db
+import segmentation.database as db
 
 def get(query):
     connection = db.connect()
@@ -34,37 +34,61 @@ def cluster(df):
     return labels, n_clusters
 
 
-def leads():
-    dataset = get( "SELECT create_date, name, contact_name, country_id, expected_revenue, email_from,probability FROM crm_lead")
-    # dataset = get( "SELECT country_id, expected_revenue, probability FROM crm_lead")
+def segment():
+    partners = get( "SELECT id, name, credit_limit, function, country_id FROM res_partner")
 
-    dataset['probability'] = dataset['probability'].fillna(dataset['probability'].mean())
-    dataset['contact_name'] = dataset['contact_name'].fillna("Luis Sante")
-    dataset['expected_revenue'] = dataset['expected_revenue'].fillna(0)
-    dataset['expected_revenue'] = dataset['expected_revenue'].map(lambda x: x if x != 0 else x + dataset['expected_revenue'].mean())
+    # rename id column to partner_id
+    partners.rename(columns={'id': 'partner_id'}, inplace=True)
 
-    # using map to replace 0 with mean of probability
+    standard = StandardScaler()    
+    partners['credit_limit'] = standard.fit_transform(partners[['credit_limit']])
+    partners['country_id'] = standard.fit_transform(partners[['country_id']])
     
-    new_data = {
-        'country_id' : dataset['country_id'],
-        'expected_revenue' : dataset['expected_revenue'],
-        'probability' : dataset['probability']
-    }
+    # encode function to numeric
+    partners['function'] = partners['function'].astype('category')
+    partners['function'] = partners['function'].cat.codes
 
-    new_data = pd.DataFrame(new_data)
 
-    standard = StandardScaler()
-    new_data['country_id'] = standard.fit_transform(new_data[['country_id']])
-    new_data['expected_revenue'] = standard.fit_transform(new_data[['expected_revenue']])
-    #new_data['probability'] = standard.fit_transform(new_data[['probability']])
 
-    dataset['cluster'], n_clusters = cluster(new_data[['country_id', 'expected_revenue']])
-    dataset = dataset.sort_values(by="cluster")
-    dataset = dataset.reset_index(drop=True)
+    leads = get( "SELECT priority, expected_revenue, date_open, date_closed, probability, partner_id FROM crm_lead")
 
-    #print(dataset.isnull().sum())
+    leads['priority'] = leads['priority'].fillna(leads['priority'].mode()[0])    
+    leads['expected_revenue'] = leads['expected_revenue'].fillna(0)
+    leads['expected_revenue'] = leads['expected_revenue'].map(lambda x: x if x != 0 else x + leads['expected_revenue'].mean())
+    # new column with date_closed - date_open
+    leads['date_diff'] = leads['date_closed'] - leads['date_open']
+    leads['date_diff'] = leads['date_diff'].dt.days
+    leads['probability'] = leads['probability'].fillna(leads['probability'].mean())
 
-    return dataset, n_clusters
+    # aggregate leads by partner_id
+    leads = leads.groupby('partner_id').agg({'priority': lambda x: x.value_counts().index[0], 'expected_revenue': 'mean', 'date_diff': 'mean', 'probability': 'mean'})
+    leads.reset_index(inplace=True)
+    
+    # rename columns to avg
+    leads.rename(columns={'priority': 'priority_mode', 'expected_revenue': 'expected_revenue_avg', 'date_diff': 'date_diff_avg', 'probability': 'probability_avg'}, inplace=True)
 
-df,c = leads()
-print(df)
+    # merge 
+    partners = pd.merge(partners, leads, on='partner_id')
+
+    #########################
+    ###      Cluster      ###
+    #########################
+    partners['cluster'], n_clusters = cluster(partners[['country_id', 'credit_limit', 'function']])
+    partners = partners.sort_values(by="cluster")
+    partners = partners.reset_index(drop=True)
+
+    # round values
+    partners['credit_limit'] = partners['credit_limit'].round(2)
+    partners['expected_revenue_avg'] = partners['expected_revenue_avg'].round(2)
+    partners['date_diff_avg'] = partners['date_diff_avg'].round(2)
+    partners['probability_avg'] = partners['probability_avg'].round(2)
+    partners['country_id'] = partners['country_id'].round(2)
+    # partners['priority_avg'] = partners['priority_avg'].round(2)
+
+    # delete partner_id column
+    partners.drop(columns=['partner_id'], inplace=True)
+
+    return partners, n_clusters
+
+# df,c = segment()
+# print(df)
