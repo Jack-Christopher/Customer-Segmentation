57a2f97b7c695bcd7c4028df31fbd9d2e855857a
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-08-16 08:42:10 -0500

    feat added table url

--- Frontend/apps/home/urls.py
+++ Frontend/apps/home/urls.py
@@ -5,11 +5,14 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
+from .views import index, pages, tables
 
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
+
+    path('tables.html', views.tables, name='tables'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
