3c358540dc477503ff8e898bc1a0f1a79b2c7eb1
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-08-16 08:37:34 -0500

    feat: added 'metrics', 'history' and 'last_year' endpoints, added CORS

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS, cross_origin
 import segmentation.database as db
 import segmentation.segment as seg
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'very-secret-key'
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
@@ -15,7 +19,7 @@ def index():
 
 @app.route('/partners', methods=['GET'])
 def partners():
-    query = "SELECT name, credit_limit, country_id FROM res_partner"
+    query = "SELECT name, function, credit_limit, country_id FROM res_partner"
     df = seg.get(query)
     return df.to_json(orient='records')
 
@@ -23,7 +27,70 @@ def partners():
 def leads():
     query = "SELECT name, priority, country_id, expected_revenue, probability, date_open, date_closed FROM crm_lead"
     df = seg.get(query)
+    
+    df['date_open'] = df['date_open'].astype(str)
+    df['date_closed'] = df['date_closed'].astype(str)
+
     return df.to_json(orient='records')
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    leads = "SELECT name, priority, country_id, expected_revenue, probability, date_open, date_closed FROM crm_lead"
+    df_leads = seg.get(leads)
+
+    # get metrics from dataframe
+    prob_avg = round(df_leads['probability'].mean(), 2)
+    exp_avg = df_leads['expected_revenue'].mean()
+    exp_total = round(df_leads['expected_revenue'].sum(), 2)
+    
+
+    partners = "SELECT name, function, credit_limit, country_id FROM res_partner"
+    df_partners = seg.get(partners)
+
+    # get metrics from dataframe
+    credit_avg = df_partners['credit_limit'].mean()
+    function_mode = df_partners['function'].mode()[0]
+    
+    data = {
+        'prob_avg': prob_avg,
+        'exp_avg': exp_avg,
+        'exp_total': exp_total,
+        'credit_avg': credit_avg,
+        'function_mode': function_mode
+    }
+
+    return jsonify(data)
+
+@app.route('/history', methods=['GET'])
+@cross_origin()
+def history():
+    query = "SELECT date_open FROM crm_lead"
+    df = seg.get(query)
+    # get the last 6 years of data
+    df = df[df['date_open'] > datetime(datetime.now().year - 6, 1, 1)]
+    # add year column
+    df['year'] = df['date_open'].dt.year
+    # get the number of leads per year and rename it to 'n_leads'
+    df = df.groupby(['year']).count()
+    # add id column
+    df['year'] = df.index
+    return df.to_json(orient='records')
+
+@app.route('/last_year', methods=['GET'])
+@cross_origin()
+def last_year():
+    query = "SELECT expected_revenue, date_open FROM crm_lead"
+    df = seg.get(query)
+    # get the last year of data
+    df = df[df['date_open'] > datetime(datetime.now().year - 1, 1, 1)]
+    print(df)
+    # get the expected revenue, group by month 
+    df = df.groupby([df['date_open'].dt.month]).sum()
+    # add id column
+    df['month'] = df.index
+    return df.to_json(orient='records')
+    
+
 
 @app.route('/segmentation', methods=['GET'])
 def segmentation():
